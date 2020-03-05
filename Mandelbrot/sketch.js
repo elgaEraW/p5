477eaf67aef524ac8e321df765be6da36e3e162d
DevJoshi030 <47146845+DevJoshi030@users.noreply.github.com>
2020-03-05 20:30:24 +0530

    -> Added coloring to Mandelbrot Set.

--- Mandelbrot/sketch.js
+++ Mandelbrot/sketch.js
@@ -1,6 +1,76 @@
 // let b;
 let cX = 0,
     cY = 0;
+let N = 1;
+let x, y;
+let store;
+var flag;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function calculate() {
+    // while (N < 500) {
+    for (var i = 0; i < width; i++) {
+        for (var j = 0; j < height; j++) {
+            // await sleep(1);
+            colorMode(HSB);
+            count = 0;
+
+            x = map(i, 0, width, -2, 2);
+            y = map(j, 0, height, -2, 2);
+
+            cX = x; //map(mouseX, 0, width, -1, 1);
+            cY = y; //map(mouseY, 0, height, -1, 1);
+            // if (!flag) {
+            //     x = store[int('' + i + j)].x;
+            //     y = store[int('' + i + j)].y;
+            //     count = N / 2;
+            //     // console.log(store);
+            // }
+            while (count < N) {
+                var newX = x * x - y * y;
+                var newY = 2 * x * y;
+
+                x = newX + cX;
+                y = newY + cY;
+
+                if (abs(x * x + y * y) > 2) {
+                    break;
+                }
+                count++;
+            }
+
+            var b = map(count, 0, N, 0, 1);
+            b = map(sqrt(b), 0, 1, 0, 255);
+            if (count == N) {
+                b = 0;
+                // store[int('' + i + j)] = { h: b, s: 0, b: 0, m: RGB };
+                colorMode(RGB);
+                stroke(b);
+            } else {
+                // store[int('' + i + j)] = { h: 255 - b, s: 255, b: 255, m: HSB };
+                // nsmooth = count + 1 - Math.log(Math.log(zn.abs())) / Math.log(2)
+                stroke(255 - b, 255, 255);
+            }
+
+            point(i, j);
+            // translate(-width / 2, 0);
+        }
+    }
+    flag = false;
+    // store = { x, y, N };
+    // loadPixels();
+    // colorMode(HSB);
+
+    N *= 2;
+    if (N > 1024) {
+        noLoop();
+    }
+    // console.log(store
+    // }
+}
 
 function keyPressed() {
     if (keyCode === UP_ARROW) {
@@ -10,52 +80,22 @@ function keyPressed() {
 }
 
 function setup() {
-    createCanvas(1500, 900);
+    createCanvas(1825, 950);
     // b = new Box();
     // background(220);
-    pixelDensity(1);
+    // pixelDensity(1);
+    // colorMode(RGB);
+    // background(0);
+    colorMode(HSB);
+
 
 }
 
 function draw() {
 
+    // translate(990, 0);
     // b.update();
     // b.show();
-    colorMode(HSB);
-    loadPixels();
-
-    for (var i = 0; i < width; i++) {
-        for (var j = 0; j < height; j++) {
-            count = 0;
-            var x = map(i, 0, width, -2, 2);
-            var y = map(j, 0, height, -2, 2);
-
-            cX = map(mouseX, 0, width, -1, 1);
-            cY = map(mouseY, 0, height, -1, 1);
-            while (count < 100) {
-                var newX = x * x - y * y;
-                var newY = 2 * x * y;
-
-                x = newX + cX;
-                y = newY + cY;
-
-                if (abs(x * x + y * y) > 16) {
-                    break;
-                }
-                count++;
-            }
-            var b = map(count, 0, 100, 0, 1);
-            b = map(sqrt(b), 0, 1, 0, 255);
-            if (count == 100) {
-                b = 0;
-            }
-
-            var pix = (i + j * width) * 4;
-            pixels[pix + 0] = b;
-            pixels[pix + 1] = b;
-            pixels[pix + 2] = b;
-            pixels[pix + 3] = 255;
-        }
-    }
-    updatePixels();
+    calculate();
+    // updatePixels();
 }
